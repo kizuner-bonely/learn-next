486f6ac6a2e9c61e7118081105d26b07eff90df1
kizuner-bonely <1003306162@qq.com>
2024-10-19 11:26:18 +0800

    chore: configure stylelint

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,5 +1,10 @@
 module.exports = {
-  // plugins: ['stylelint-prettier'],
-  extends: ['stylelint-config-tailwindcss', 'stylelint-prettier/recommended'],
-  // rules: { 'prettier/prettier': true },
+  plugins: ['stylelint-prettier'],
+  extends: [
+    'stylelint-config-tailwindcss',
+    'stylelint-prettier/recommended',
+    'stylelint-config-recess-order',
+    'stylelint-config-prettier',
+  ],
+  rules: { 'prettier/prettier': true },
 }
