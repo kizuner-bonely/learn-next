48ed648a29ca38351d8307e16dd53841bb235d6e
kizuner-bonely <1003306162@qq.com>
2024-10-20 14:42:56 +0800

    feat: use hydrateRoot to improve ssr

--- packages/4-ssr/client.js
+++ packages/4-ssr/client.js
@@ -1,7 +1,6 @@
 import React from 'react'
-import { createRoot } from 'react-dom/client'
+import { hydrateRoot } from 'react-dom/client'
 
 import App from './pages'
 
-const root = createRoot(document.getElementById('root'))
-root.render(<App />)
+hydrateRoot(document.getElementById('root'), <App />)
