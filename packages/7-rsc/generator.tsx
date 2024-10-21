6988e295842d8058f63e725fb8d9bffab1c08600
kizuner-bonely <1003306162@qq.com>
2024-10-21 21:14:12 +0800

    feat: implement custom components

--- packages/7-rsc/generator.tsx
+++ packages/7-rsc/generator.tsx
@@ -2,31 +2,14 @@
 import { readFile } from 'fs/promises'
 import React from 'react'
 
+import { BlogPostPage } from './components'
 import { renderJSXToHTML } from './utils'
 
 export async function generateHtml() {
   const author = 'hh'
   const postContent = await readFile('./posts/hello.txt', 'utf-8')
 
-  const jsx = (
-    <html>
-      <head>
-        <title>My blog</title>
-        <script async src="https://cdn.tailwindcss.com"></script>
-      </head>
-      <body className="p-5">
-        <nav className="flex items-center justify-center gap-10 text-blue-600">
-          <a href="/">Home</a>
-        </nav>
-        <article className="h-40 mt-5 flex-1 rounded-xl bg-indigo-500 text-white flex items-center justify-center">
-          {postContent}
-        </article>
-        <footer className="h-20 mt-5 flex-1 rounded-xl bg-cyan-500 text-white flex items-center justify-center">
-          (c) {author}, {new Date().getFullYear()}
-        </footer>
-      </body>
-    </html>
+  return renderJSXToHTML(
+    <BlogPostPage postContent={postContent} author={author} />,
   )
-
-  return renderJSXToHTML(jsx)
 }
--- packages/7-rsc/utils.ts
+++ packages/7-rsc/utils.ts
@@ -13,20 +13,34 @@ export function renderJSXToHTML(jsx: unknown) {
     return jsx.map((child) => renderJSXToHTML(child)).join('')
   } else if (isObject(jsx)) {
     if (jsx.$$typeof === Symbol.for('react.element')) {
-      let html = `<${jsx.type}`
-      for (const propName in jsx.props) {
-        if (jsx.props.hasOwnProperty(propName) && propName !== 'children') {
-          html += ' '
-          html += propName
-          html += '='
-          html += `"${escapeHtml(jsx.props[propName])}"`
+      // normal html element
+      if (typeof jsx.type === 'string') {
+        let html = `<${jsx.type}`
+        for (const propName in jsx.props) {
+          if (jsx.props.hasOwnProperty(propName) && propName !== 'children') {
+            html += ' '
+            html += propName
+            html += '='
+            html += `"${escapeHtml(jsx.props[propName])}"`
+          }
         }
+        html += '>'
+        html += renderJSXToHTML(jsx.props.children)
+        html += `</${jsx.type}>`
+        html = html.replace(/className/g, 'class')
+        return html
       }
-      html += '>'
-      html += renderJSXToHTML(jsx.props.children)
-      html += `</${jsx.type}>`
-      html = html.replace(/className/g, 'class')
-      return html
+      // custom component
+      else if (typeof jsx.type === 'function') {
+        const Component = jsx.type
+        const props = jsx.props
+        const returnedJsx = Component(props)
+        return renderJSXToHTML(returnedJsx)
+      }
+      // fallback
+      else {
+        throw new Error('Type not implemented.')
+      }
     } else {
       throw new Error('Cannot render an object.')
     }
