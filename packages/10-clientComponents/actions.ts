15a4af7f0941b7219052075322749143978da5a1
kizuner-bonely <1003306162@qq.com>
2024-10-24 21:52:30 +0800

    feat: commit client component temporarily

--- packages/10-clientComponents/actions.ts
+++
@@ -1,7 +0,0 @@
-export default async function serverAction(req, res) {
-  const action = req.url.slice(9) // remove '/actions/'
-  const _module = await import(`./actions/${action}.js`)
-  const actionFunction = _module.default
-  await actionFunction(req, res)
-  res.redirect(302, `/${req.body.slug}`)
-}
