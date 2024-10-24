ab9412296027b16961d191f51fa68ac4c6db8c77
kizuner-bonely <1003306162@qq.com>
2024-10-24 21:09:44 +0800

    feat: implement client component compilation

--- packages/10-clientComponents/client.js
+++ packages/10-clientComponents/client.js
@@ -1,6 +1,8 @@
 import { createElement, use, useState, startTransition } from 'react'
 import { createFromFetch } from 'react-server-dom-webpack'
+import { readFile, writeFile } from 'fs/promises'
 import { hydrateRoot } from 'react-dom/client'
+import path from 'path'
 
 const clientJSXCache = {}
 let currentPathname = window.location.pathname
@@ -9,6 +11,90 @@ let updateRoot = null
 const data = createFromFetch(fetch(`${currentPathname}?jsx`))
 
 hydrateRoot(document, createElement(Shell, { data }))
+
+export async function renderJSXToClientJSX(jsx) {
+  if (
+    typeof jsx === 'string' ||
+    typeof jsx === 'number' ||
+    typeof jsx === 'boolean' ||
+    jsx === null
+  ) {
+    return jsx
+  } else if (Array.isArray(jsx)) {
+    return Promise.all(jsx.map((child) => renderJSXToClientJSX(child)))
+  } else if (jsx !== null && typeof jsx === 'object') {
+    if (jsx.$$typeof === Symbol.for('react.element')) {
+      if (typeof jsx.type === 'string') {
+        return {
+          ...jsx,
+          props: await renderJSXToClientJSX(jsx.props),
+        }
+      } else if (typeof jsx.type === 'function') {
+        const Component = jsx.type
+        const props = jsx.props
+        const isClientComponent = Component.toString().includes('use client')
+        if (isClientComponent) {
+          return await transformClientComponent(Component, props)
+        } else {
+          const returnedJSX = await Component(props)
+          return renderJSXToClientJSX(returnedJSX)
+        }
+      } else {
+        throw new Error('unknown jsx.type')
+      }
+    } else {
+      return Object.fromEntries(
+        await Promise.all(
+          Object.entries(jsx).map(async ([propName, value]) => [
+            propName,
+            await renderJSXToClientJSX(value),
+          ]),
+        ),
+      )
+    }
+  } else {
+    throw new Error('type not implemented')
+  }
+}
+
+async function transformClientComponent(Component, props) {
+  const raw = Component.toString()
+  const children = await renderJSXToClientJSX(props.children)
+
+  const clientComponent = {
+    value: raw,
+    props: {
+      ...props,
+      'data-client': true,
+      'data-component': Component.name,
+      children,
+    },
+  }
+
+  await createClientComponentJS(clientComponent)
+
+  return createElement('div', {
+    'data-client': true,
+    'data-component': Component.name,
+  })
+}
+
+async function createClientComponentJS(Component) {
+  const { props, value } = Component
+  const name = props['data-component']
+  const filenameRaw = path.join(process.cwd(), 'public', 'client', `${name}.js`)
+  const filename = path.normalize(filenameRaw)
+  const fileContents = `
+import React from 'react'
+export const props = ${JSON.stringify(props)}
+export const jsx = ${value.replaceAll('import_react.default', 'React')}
+`
+  try {
+    await writeFile(filename, fileContents)
+  } catch (err) {
+    console.error('err in writeComponentToDisk', err)
+  }
+}
 
 function Shell({ data }) {
   console.log('Shell', data)
